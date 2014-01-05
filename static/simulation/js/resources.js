a6f602a63abe3329a09506d15c6ac8a95bad0e2e
fukangwen <fukangwen@gmail.com>
2014-01-05 02:39:28 -0500

    map prototype done

---
+++ static/simulation/js/resources.js
@@ -0,0 +1,20 @@
+game.resources = [
+	/**
+	 * Graphics.
+	 */
+	// our level tileset
+	{name: "chapter",  type:"image",	src: "data/img/map/chapter.png"},
+	
+	/* 
+	 * Maps. 
+ 	 */
+	{name: "map_prototype",              type: "tmx",	src: "data/map/map_prototype.tmx"}
+
+	/* 
+	 * Background music. 
+	 */	
+	
+	/* 
+	 * Sound effects. 
+	 */
+];
