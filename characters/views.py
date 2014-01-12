3dc561e2abdf32ef40d0c939496572d53c5ce279
Cheng Xing <xc561@hotmail.com>
2014-01-12 15:05:05 -0500

    backbone test page works: removed restframework pagination setting

--- characters/views.py
+++ characters/views.py
@@ -7,7 +7,7 @@ from serializers import CharacterSerializer
 
 class CharacterViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows characters to be viewed or edited.
     """
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
